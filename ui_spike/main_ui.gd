bfa6c40054e6fbb2992b7f8fae00a9ceddef7b0a
Northburns <northburns@gmail.com>
2018-11-05 00:51:55 +0200

    hmm

--- ui_spike/main_ui.gd
+++ ui_spike/main_ui.gd
@@ -1,5 +1,6 @@
 extends MarginContainer
 
+onready var vpcont1 = $VBoxContainer/VpCont_Main
 onready var viewport1 = $VBoxContainer/VpCont_Main/Viewport
 onready var viewport2 = $VBoxContainer/VpCont_Goal/Viewport
 onready var camera1 = $VBoxContainer/VpCont_Main/Viewport/colworld/player/Camera2D
@@ -16,4 +17,31 @@ func _ready():
 	var target = $VBoxContainer/VpCont_Main/Viewport/colworld/princess
 	assert(target != null)
 	camera2.target = target
+	
+	get_tree().get_root().connect("size_changed", self, "myfunc")
 
+
+
+func _on_MarginContainer_resized():
+	print("HAHAHAAA!")
+	myfunc()
+	pass # replace with function body
+
+func myfunc():
+	
+	print("Whoa! " + String(get_viewport_rect().size))
+	print("AAAND " + String(get_rect().size))
+	print("WINDOWS: " + String(OS.get_window_size()))
+	if viewport1 != null:
+		print("WWWWW " + String(viewport1.get_visible_rect()))
+#		viewport1.set_size_override(true, OS.get_window_size())
+#		viewport1.set_size_override_stretch(true)
+	
+		viewport1.remove_child(world)
+		viewport1.remove_and_skip()
+		var vp = Viewport.new()
+		vp.add_child(world)
+		vpcont1.add_child(vp)
+		viewport1 = vp
+	
+	
