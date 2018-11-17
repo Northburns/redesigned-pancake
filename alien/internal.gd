cc3cda4abbdd5781c280b589216526635ca2594c
Northburns <northburns@gmail.com>
2018-11-18 00:37:41 +0200

    oh my

--- alien/internal.gd
+++ alien/internal.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-onready var pl = $"../AnimationPlayer"
+onready var pl = $"../animation/AnimationPlayer"
 
 func _ready():
 	print("Oh my!")
-	pl.play("Walk right")
+	pl.play("walk")
