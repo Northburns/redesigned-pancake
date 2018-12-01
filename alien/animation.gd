413da0f78e2823628b7efe5857b58919dad93edb
Northburns <northburns@gmail.com>
2018-12-01 21:17:52 +0200

    unused files move

--- alien/animation.gd
+++ alien/animation.gd
@@ -5,13 +5,7 @@ onready var drone = $drone
 var steps = []
 
 func _ready():
-	for s in get_node("StepSounds").get_children():
-		steps.append(s)
 	set_drone_visible(false)
-
-func step():
-	var s = steps[randi()%steps.size()]
-	s.play()
 
 func set_drone_visible(v):
 	drone.visible = v
